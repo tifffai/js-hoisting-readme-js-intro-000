ccce0f4891993462705080ce0470e9ab947c3432
Tiffany Fai <tbluebox@gmail.com>
2019-11-09 12:35:00 +0000

    Automatically backed up by Learn

--- hoisting.js
+++ hoisting.js
@@ -20,10 +20,9 @@ function sayMyName() {
   // fix the code in here:
   var name = "Cricky";
 
-  sayMy();
-
   function sayMy() {
     var name = "Kristin";
     console.log(name);
   }
+  sayMy();
 }
